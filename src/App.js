286ab43a77a7fa3659e65bda46e5a2f3f88969a5
Rachel Eiting <reiting1@gmail.com>
2023-09-16 13:41:41 -0400

    setting up basic structure of header and search bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import yousicianhero from './assets/yousician-hero.png'
+import search from './assets/icons/search.svg'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <img src={yousicianhero} className="App-logo" alt="yousician header" />
+      <div className="input">
+        <input type="text">
+        </input>
+        <button>
+        <img src={search} className="search-box" alt="search box" />
+
+        </button>
+      </div>
+    </header>
   );
 }
 
